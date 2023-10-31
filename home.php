dd0822eeeb2756a6ab053e1cc1f8c159e0f5c4a6
Recep Yildiz <recep.yildiz@student.rocva.nl>
2023-10-31 23:27:02 +0100

    script js file geconnect met home

--- home.php
+++ home.php
@@ -11,7 +11,6 @@
   <!-- css file link -->
   <link rel="stylesheet" href="style.css">
 
-
 </head>
 <body>  
   
@@ -82,31 +81,7 @@
   <!-- home sectie eindigt -->
 
 </body>
-<script>
-  // Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById('openModal');
+<script src="script.js"></script> 
 
-// Get the <span> element that closes the modal
-var span = document.getElementById('closeModal');
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = 'block';
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = 'none';
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-</script>
 </html>
