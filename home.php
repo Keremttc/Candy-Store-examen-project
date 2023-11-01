ee9b8911c0ea68593c4864590a485b668a7015ba
Kerem Tutucu <Keremtutucu@gmail.com>
2023-11-01 11:51:53 +0100

    Footer toegevoegd

--- home.php
+++ home.php
@@ -80,8 +80,66 @@
 
   <!-- home sectie eindigt -->
 
+
+  <!-- footer sectie begint -->
+
+    <footer class="footer">
+      <div class="col">
+        <h4>contact</h4>
+        <p><strong>Adres:</strong> Westcoast</p>
+        <p><strong>Telefoonnummer:</strong> 05494939</p>
+        <p><strong>Uren beschikbaar:</strong> 10:00 - 17:00 Ma - Vrij</p>
+      </div>
+
+
+      <div class="col">
+        <h4>About</h4>
+        <a href="#">Over ons</a>
+        <a href="#">Verzend informatie</a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms & Conditions</a>
+        <a href="contact.php">Contacteer ons</a>
+      </div>
+
+      
+      <div class="col">
+        <h4>Mijn account</h4>
+        <a href="#">Log in</a>
+        <a href="#">Bekijk winkelwagen</a>
+        <a href="#">Bekijk favorieten</a>
+        <a href="#">Track mijn order</a>
+        <a href="contact.php">Help</a>
+      </div>
+
+      <div class="copyright">
+        <p> © 2023, Chopper's Candy-Store - HTML PHP CSS JAVASCRIPT</p>
+        <div>
+
+
+
+    </footer>
+
+
+
+
+
+<!-- footer sectie eindigt -->
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 
 <script src="script.js"></script> 
 
 </html>
+
+
